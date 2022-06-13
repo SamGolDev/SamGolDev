a5a13cc1707dc8a1ec0845449b13c106cb9fbc43
Sergey <fesachedidit@gmail.com>
2022-06-14 01:40:05 +0300

    added new sections

--- src/js/script.js
+++ src/js/script.js
@@ -1,3 +1,4 @@
+        /* бургер меню */
 const burger = document.querySelector('.burger'),
 	menu = document.querySelector('.menu'),
 	close = document.querySelector('.menu__close');
@@ -8,4 +9,25 @@ burger.addEventListener('click', () => {
 
 close.addEventListener('click', () => {
 	menu.classList.remove('active');
+});
+		/* закрытие меню после перехода к секции */
+menu.querySelectorAll('a').forEach(link => {
+	link.addEventListener('click', () => {
+		close.classList.toggle('active');
+		menu.classList.toggle('active');
+	});
+});
+
+
+
+			/* SmoothScroll */
+new SmoothScroll('a[href*="#"]', {
+	speed: 400
+});
+			/* пересчет процентов */
+const counters = document.querySelectorAll ('.skills__ratings-counter'),
+	lines = document.querySelectorAll ('.skills__ratings-line span');
+
+counters.forEach( (item, i) => {
+	lines[i].style.width = item.innerHTML;
 });
